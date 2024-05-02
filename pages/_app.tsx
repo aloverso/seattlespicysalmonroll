229bfda33776e9f8a5fd762d27a9a09dbc6e55fa
Anne LoVerso <anne.loverso@gmail.com>
2024-05-02 01:41:50 -0400

    replace gtag with g-analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,15 @@ const App = ({ Component, pageProps }: AppProps): ReactElement => {
   return (
     <>
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K1FH7N53LK"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WN26N2XC');
+            window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-K1FH7N53LK');
         `,
           }}
         />
