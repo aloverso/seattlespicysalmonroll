51ab95bfc381ad9e3eb9874c1ad4b1b14ff2a3de
Anne LoVerso <anne.loverso@gmail.com>
2024-05-02 01:29:50 -0400

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,17 @@ const App = ({ Component, pageProps }: AppProps): ReactElement => {
   return (
     <>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WN26N2XC');
+        `,
+          }}
+        />
         <script src="https://kit.fontawesome.com/636812bbf4.js" crossOrigin="anonymous"></script>
         <title>Seattle Spicy Salmon Roll</title>
       </Head>
