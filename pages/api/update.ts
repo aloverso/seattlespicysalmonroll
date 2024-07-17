f8e13a49fea2167502f997593256d3c3d64651d3
Anne LoVerso <anne.loverso@gmail.com>
2024-07-17 16:51:22 -0400

    add api endpoints

---
+++ pages/api/update.ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import * as nacl from "tweetnacl"
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  if (!isVerified(req)) {
+    return res.status(401).end("invalid request signature");
+  }
+
+  if (req.method === 'POST') {
+    if (req.body["type"] === 1) {
+      res.status(200).json({ type: 1 })
+    }
+    else {
+      res.status(200).json({ message: "nothing here yet - post" })
+    }
+  } else {
+      res.status(200).json({ message: "nothing here yet - other methods" })
+  }
+}
+
+const isVerified = (req: NextApiRequest): boolean => {
+  const PUBLIC_KEY = "37f4b808785ce0f2a558e5717c952f430ddf1bbac2fa85319952d7b4effa6ca3";
+
+  const signature = req.headers["X-Signature-Ed25519"] as string;
+  const timestamp = req.headers["X-Signature-Timestamp"] as string;
+
+  if (!signature || !timestamp) return false;
+
+  const rawBody = JSON.stringify(req.body);
+
+  return  nacl.sign.detached.verify(
+    Buffer.from(timestamp + rawBody),
+    Buffer.from(signature, "hex"),
+    Buffer.from(PUBLIC_KEY, "hex")
+  );
+}
+
+
