b16bd74c85c8fb648bc351daba74ccbd24447a11
Anne LoVerso <anne.loverso@gmail.com>
2024-07-17 22:42:37 -0400

    updatr grammar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -46,15 +46,15 @@ export const Hero = (): ReactElement => {
     <div className="fdr text-white hero-countdown fjc">
       <div className="fdc fac hero-countdown-item">
         <div className="hero-number">{timeUntil.day}</div>
-        <div className="hero-label">{timeUntil.day > 1 ? "days" : "day"}</div>
+        <div className="hero-label">{timeUntil.day === 1 ? "day" : "days"}</div>
       </div>
       <div className="fdc fac hero-countdown-item">
         <div className="hero-number">{timeUntil.hour}</div>
-        <div className="hero-label">{timeUntil.hour > 1 ? "hours" : "hour"}</div>
+        <div className="hero-label">{timeUntil.hour === 1 ? "hour" : "hours"}</div>
       </div>
       <div className="fdc fac hero-countdown-item">
         <div className="hero-number">{timeUntil.minute}</div>
-        <div className="hero-label">{timeUntil.minute > 1 ? "minutes" : "minute"}</div>
+        <div className="hero-label">{timeUntil.minute === 1 ? "minute" : "minutes"}</div>
       </div>
     </div>
   )
