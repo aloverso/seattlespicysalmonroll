a7532bed4548f112a7c8737d50ad72fac91c8b78
Anne LoVerso <anne.loverso@gmail.com>
2025-07-21 12:47:08 -0400

    announcement

--- src/components/LiveUpdates.tsx
+++ src/components/LiveUpdates.tsx
@@ -30,6 +30,13 @@ export const LiveUpdates = (): ReactElement => {
         timestamp: "2025-07-19T17:07:12-04:00",
         id: "3",
       },
+      {
+        message:
+          "Thanks for participating! Check your email or the WhatsApp group for the link to" +
+          "upload your photos and videos to our shared folder.",
+        timestamp: "2025-07-21T09:44:12-04:00",
+        id: "4",
+      },
     ]);
     // fetch("/api/getstore").then((response) => {
     //   response.json().then((json) => {
@@ -64,24 +71,24 @@ export const LiveUpdates = (): ReactElement => {
         .
       </p>
       <div className="pvs feed">
-        <div className="brad bg-white-overlay pad mvs text-black ">
-          <div className="fdr fjc fac mbs">
-            <img src="/icons/thumbtack.svg" alt="PIN" style={{ width: "14px", height: "14px" }} />
-            <div className="text-s text-uppercase mlxs">PINNED</div>
-          </div>
-          <div>
-            Got questions? Want real-time updates? Connect with your fellow SSSR skaters on our{" "}
-            <a className="bold" href="https://chat.whatsapp.com/ERZYFz64NC13ZhiopBL3oQ?mode=r_c">
-              WhatsApp group
-            </a>
-            !
-          </div>
-        </div>
+        {/*<div className="brad bg-white-overlay pad mvs text-black ">*/}
+        {/*  <div className="fdr fjc fac mbs">*/}
+        {/*    <img src="/icons/thumbtack.svg" alt="PIN" style={{ width: "14px", height: "14px" }} />*/}
+        {/*    <div className="text-s text-uppercase mlxs">PINNED</div>*/}
+        {/*  </div>*/}
+        {/*  <div>*/}
+        {/*    Got questions? Want real-time updates? Connect with your fellow SSSR skaters on our{" "}*/}
+        {/*    <a className="bold" href="https://chat.whatsapp.com/ERZYFz64NC13ZhiopBL3oQ?mode=r_c">*/}
+        {/*      WhatsApp group*/}
+        {/*    </a>*/}
+        {/*    !*/}
+        {/*  </div>*/}
+        {/*</div>*/}
         {updates
-          .sort((a, b) => dayjs(a.timestamp).isAfter(dayjs(b.timestamp)) ? -1 : 1)
+          .sort((a, b) => (dayjs(a.timestamp).isAfter(dayjs(b.timestamp)) ? -1 : 1))
           .map((update) => (
-          <Update key={update.id} update={update} />
-        ))}
+            <Update key={update.id} update={update} />
+          ))}
       </div>
     </div>
   );
